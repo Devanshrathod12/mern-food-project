81a2b5fa0ef326814a76444922a6828b20ddb635
Devanshrathod12 <141814605+Devanshrathod12@users.noreply.github.com>
2024-09-01 02:28:49 +0530

    Update App.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -10,7 +10,8 @@ import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
  
-  const url = "http://localhost:4000";
+  //const url = "http://localhost:4000";
+ const url = "https://mern-backend-44px.onrender.com";
 
   return (
     <div>
